ab8ba418edaad39967bceeeb6784881b986561bf
Jordan <46093640+searcyhosea@users.noreply.github.com>
2018-12-23 11:55:41 -1000

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,38 @@
 #include <iostream>
+
+int main(int argc, char *argv[])
+{
+  while (true) {
+    
+    std::cout << "First Number: ";
+    std::cin >> NumberOne;
+    std::cout << endl;
+
+    std::cout << "Mathematical Operator: ";
+    std::cin >> Operator;
+    std::cout << endl;
+
+    std::cout << "Second Number: ";
+    std::cin >> NumberTwo;
+    std::cout << endl;
+
+    if (Operator == '+') {
+      Result = NumberOne + NumberTwo;
+      printf("%s", Result);
+    }
+    else if (Operator == '-') {
+      Result = NumberOne - NumberTwo;
+      printf("%s", Result);
+    }
+    else if (Operator == '*') {
+      Result = NumberOne * NumberTwo;
+      printf("%s", Result);
+    }
+    else if (Operator == '/') {
+      Result = NumberOne / NumberTwo;
+      printf("%s", Result);
+    }
+    
+  }
+  return 0; 
+}
